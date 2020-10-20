15ff5b0cfc7b6e1a66aa3370a38bafdb7b4604d0
Erik Ellis <erik.ellis@royaldesign.com>
2020-10-20 09:46:00 +0200

    Remove unused sample code.

--- Ellis.Rate/ClientApp/src/app/app.module.ts
+++ Ellis.Rate/ClientApp/src/app/app.module.ts
@@ -4,22 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { RateComponent } from "./rate/rate.component";
+import { RateComponent } from './rate/rate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     RateComponent
   ],
   imports: [
@@ -29,10 +21,7 @@ import { RateComponent } from "./rate/rate.component";
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'rate', component: RateComponent },
+      { path: '', component: RateComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
